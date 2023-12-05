2a7358b2d5fce86cd8c3f01ed7cf038ba4c40c36
Karen Ziv <me@karenziv.com>
2023-12-05 05:20:16 -0500

    Updated stores example for clarity

--- examples/stores.js
+++ examples/stores.js
@@ -4,12 +4,13 @@ const publixConnector = require('@kziv/publix-connector');
 // Instantiate the Stores connector.
 const Stores = new publixConnector.Stores({
   shouldParseAddresses: true,
+  // This is only needed if shouldParseAddresses is `true`.
   addressParserApiKey: 'YOUR-GEOCOD.IO-API-KEY',
 });
 
 // Get stores for a given location, promise style.
 // You can also use async/await syntax.
-Stores.getStores('some sort of address')
+Stores.getStores('Orlando, FL')
   .then((data) => {
     console.log(data);
   });
